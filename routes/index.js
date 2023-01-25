e8827b3b47ac3344e5243e2f6670e09a3f37f75b
motawallison <wmotadev@gmail.com>
2023-01-25 19:02:22 -0300

    adicionando estoque

--- routes/index.js
+++ routes/index.js
@@ -31,8 +31,8 @@ router.post('/addProduto', (req, res) => {
 
 })
 
-router.get('/editar/:id', (req, res) => {
-  db.query('SELECT * FROM produtos WHERE id = ?', [req.params.id], function(erro, resultado){
+router.get('/editar/:IDPROD', (req, res) => {
+  db.query('SELECT * FROM produtos WHERE IDPROD = ?', [req.params.IDPROD], function(erro, resultado){
     if(erro){
       res.status(200).send('Erro: ' + erro)
     }
@@ -40,8 +40,8 @@ router.get('/editar/:id', (req, res) => {
   })
 })
 
-router.post('/edit/:id', (req, res) => {
-  db.query('UPDATE produtos SET nome = ?, preco = ? WHERE id = ?', [req.body.nome, req.body.preco, req.params.id], 
+router.post('/edit/:IDPROD', (req, res) => {
+  db.query('UPDATE produtos SET nome = ?, preco = ? WHERE IDPROD = ?', [req.body.nome, req.body.preco, req.params.IDPROD], 
   function(erro){
     if(erro){
       res.status(200).send('Erro: ' + erro)
@@ -50,8 +50,8 @@ router.post('/edit/:id', (req, res) => {
   })
 })
 
-router.delete('/delete/:id', (req, res) => {
-  db.query('DELETE FROM produtos WHERE id = ?', [req.params.id], 
+router.delete('/delete/:IDPROD', (req, res) => {
+  db.query('DELETE FROM produtos WHERE IDPROD = ?', [req.params.IDPROD], 
   function(erro){
     if(erro){
       res.status(200).send('Erro: ' + erro)
